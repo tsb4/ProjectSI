7262ec6e8379b757bc1690a6dff9efe1ddd27155
João Felix <jvbf@cin.ufpe.br>
2021-03-11 13:15:12 -0300

    desvia parede

--- test.py
+++ test.py
@@ -429,10 +429,32 @@ class App(tk.Tk):
             #else:
             #   pass
 
+    def desvia_parede(self):
+        if(self.robot_dir == 0): #UP
+            self.robot_dir = 1
+
+        if(self.robot_dir == 1): #RIGHT
+            self.turn_right()
+            for i in range(3):
+                self.after(1000)
+                if(self.looking_left() == self.looking_right() == self.looking_forward() == 6):
+                    self.turn_right()
+                    self.turn_right()
+                else:
+                    self.move_forward()
+
+        if(self.robot_dir == 2): #DOWN
+            self.robot_dir = 2
+
+        if(self.robot_dir == 3): #LEFT
+            self.robot_dir = 0
+        return
+
     def on_start_button_first_part(self, event):
         self.matrizBase = list(map(list, self.Matrix))
         self.start_first_part()
 
+    #Função que inicia a parte 2
     def start_second_part(self):
         if(not self.firstPart):
             self.after(1000, self.start_second_part)
